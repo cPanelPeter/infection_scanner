fb8a89efd133982e3de1d1e2b2f109f3cf520fd2
Peter Elsner (cPanelPeter) <peter.elsner@cpanel.net>
2015-10-08 17:09:51 -0500

    fix install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,14 @@ echo "Installing infection_scanner"
 mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/infection_scanner
 
 # Get the plugin files from Github
-curl -s https://raw.githubusercontent.com/cPanelPeter/infection_scanner/master/is_files.tar.gz > /usr/local/cpanel/base/frontend/paper_lantern/infection_scanner/is_files.tar.gz
+curl -s https://raw.githubusercontent.com/cPanelPeter/infection_scanner/master/is_files.tar.gz > /root/is_files.tar.gz
 
 # Uncompress the archive
-tar xzf /usr/local/cpanel/base/frontend/paper_lantern/infection_scanner/is_files.tar.gz
+tar xzf is_files.tar.gz
+
+# Move files to /usr/local/cpanel/base/frontend/paper_lantern/infection_scanner directory
+mv /root/infection_scanner.live.pl /usr/local/cpanel/base/frontend/paper_lantern/infection_scanner
+mv /root/infection_scanner.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/infection_scanner
 
 # Install the plugin (which also places the png image in the proper location)
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/infection_scanner/infection_scanner.tar.gz
